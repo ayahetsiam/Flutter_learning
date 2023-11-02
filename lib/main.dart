8ead971d77526ade478cbb869a9e1e4d7fc8d05a
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2023-11-02 22:56:50 +0000

    FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -29,45 +29,43 @@ class MyHomePageStata extends StatefulWidget {
   State<MyHomePageStata> createState() => _MyHomePageStataState();
 }
 
+
 class _MyHomePageStataState extends State<MyHomePageStata> {
-  int note = 0;
+  String heure = '';
+
+  void onShow()
+  {
+    setState(() {
+            heure = DateTime.now().toString();
+          });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('textButton & IconButton', style: TextStyle(color: Colors.black),),
+        title: const Text('floatingActionButton'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          TextButton(
-            onPressed: (){
-              debugPrint("le boutton est pressé");
-            }, 
-            child: const Text('TextButton', style: TextStyle(fontSize: 30),)),
-            
-            IconButton(
-              onPressed: (){
-                setState(() {
-                  note++;
-                }); 
-              }, 
-              icon: const Icon(Icons.thumb_up, color: Colors.green, size: 50,),
-               tooltip: 'j\'aime',
-               ),
-
-            Text('Votre point de vue : $note', style: TextStyle(color: Color.fromARGB(255, 69, 126, 169), fontSize: 30),),
-
-            IconButton(
-              onPressed: (){
-                setState(() {
-                  note--;
-                }); 
-              }, 
-              icon: const Icon(Icons.thumb_down, color: Colors.red, size: 50,),
-               tooltip: 'j\'aime pas',
-               ),
-        ]),
+        children: [
+          Text(
+            'date &heure = $heure',
+            style: const TextStyle(
+              color: Colors.blue,
+              fontSize: 40,
+            ),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          onShow();
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(
+          Icons.timer,
+        ),
+      ),
     );
   }
 }
