f68d2e5c983030c13c8d6e42e63de965ef7bdd6b
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2023-11-05 18:49:30 +0000

    slider

--- lib/main.dart
+++ lib/main.dart
@@ -29,46 +29,66 @@ class MyHomePageStata extends StatefulWidget {
   State<MyHomePageStata> createState() => _MyHomePageStataState();
 }
 
-
 class _MyHomePageStataState extends State<MyHomePageStata> {
   bool val1 = false;
   bool val2 = false;
+  double val3 = 0.0;
 
-  void change1(bool b){
+  void change1(bool b) {
     setState(() {
-      val1=b;
+      val1 = b;
     });
   }
 
-  void change2(bool t){
-      setState(() {
-        val2=t;
-      });
-    }
+  void change3(double b) {
+    setState(() {
+      val3 = b;
+    });
+  }
+
+  void change2(bool t) {
+    setState(() {
+      val2 = t;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Slider & Switch", ),
-      ),
-
-      body: Center(
-        child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Switch(value: val1, onChanged: change1, activeColor: Colors.green,),
-
+        appBar: AppBar(
+          title: const Text(
+            "Slider & Switch",
+          ),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Switch(
+              value: val1,
+              onChanged: change1,
+              activeColor: Colors.green,
+            ),
             SwitchListTile(
-              value: val2, 
+              value: val2,
               onChanged: change2,
               activeColor: Colors.red,
-              title: const Text("interrupteur", style: TextStyle(color: Colors.red),),
-              )
-        ]),
-      )
-    );
-    
-    
+              title: const Text(
+                "interrupteur",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text(
+              "le curseur ${(val3*100).round()}",
+              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            Slider(
+              value: val3,
+              onChanged: change3,
+              activeColor: Colors.black,
+            )
+          ]),
+        ));
   }
 }
