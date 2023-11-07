3126bdc69a7c4a77c0090ffeb3465a0a70219ed0
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2023-11-07 07:45:05 +0000

    personaliser son AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
-  
   const MyApp({super.key});
-
- 
 
   // This widget is the root of your application.
   @override
@@ -19,8 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         // useMaterial3: true,
-        primarySwatch: Colors.brown,
+        //primarySwatch: Colors.brown,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePageStata(),
     );
   }
@@ -29,56 +26,64 @@ class MyApp extends StatelessWidget {
 class MyHomePageStata extends StatefulWidget {
   const MyHomePageStata({super.key});
 
-  
   @override
   State<MyHomePageStata> createState() => _MyHomePageStataState();
 }
 
 class _MyHomePageStataState extends State<MyHomePageStata> {
-  //final nomController=TextEditingController();
-String nom = "";
-
-void afficher(String a){
-  setState(() {
-     nom = "bienvenu $a";
-  });
-    // print(nom);
+  int value = 0;
+  void augmenter(){
+    setState(() {
+      value++;
+    });
   }
-
-  void submit(String a){
-  setState(() {
-     nom = "vous avez envoyé: $a";
-  });
-    // print(nom);
+  void diminuer(){
+    setState(() {
+      value--;
+    });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //centerTitle: true,
         title: const Text(
-          "Textfield",
+          "Fichier",
+          style: TextStyle(color: Colors.black,
+          fontSize: 30,fontWeight: FontWeight.bold),
         ),
+        leading: const Icon(Icons.file_open,
+        size: 40,
+        color: Colors.black,),
+        actions:[
+          
+
+          IconButton(
+          onPressed:(){
+            augmenter();
+          }, 
+          icon: const Icon(Icons.upload, 
+          size: 40,
+          color: Colors.black,),),
+          
+          IconButton(
+          onPressed:(){
+            diminuer();
+          }, 
+          icon: const Icon(Icons.download, 
+          size: 40,
+          color: Colors.black,),),
+          
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(nom, style: const TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),),
-            TextField(
-              //controller: nomController,
-              decoration: const InputDecoration(
-                labelText: 'Nom',
-                icon: Icon(Icons.person, size: 50,),
-                iconColor: Color.fromARGB(255, 10, 12, 12),
-              ),
-              keyboardType: TextInputType.text,
-              onChanged: afficher,
-              onSubmitted: submit,
-
-            )
-          ],
-        ),
+          children: [
+           const Text('Faisons un text en même temps'),
+            const Padding(padding: EdgeInsets.only()),
+            Text(value.toString()),
+          ]),
       ),
     );
   }
