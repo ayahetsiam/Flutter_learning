55bb2df6b179608bde431bb8373b054585a39583
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2023-11-10 05:47:49 +0000

    toujours la barre de menu

--- lib/main.dart
+++ lib/main.dart
@@ -32,89 +32,23 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     
     return Scaffold(
-      appBar: AppBar(
-        title: Text(val),
+      appBar: AppBar(),
+      drawer: const Drawer(backgroundColor: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          DrawerHeader(
+            child: Text('Menu')),
+            ListTile(
+              title: Text(
+                "Acueil", 
+                selectionColor: Colors.brown,
+              ),
+              onTap: null,
+              leading: Icon(Icons.home),
+            )
+      ]),
       ),
-      drawer: Drawer(
-        backgroundColor: Color.fromARGB(255, 229, 18, 18),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const DrawerHeader(
-              child: Text(
-                "La liste des enfants",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),),
-              ),
-              ListTile(
-                title: const Text("Etsiam",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,)),
-                onTap: (){
-                  setState(() {
-                    val="Etsiam";
-                  });
-                },
-              ),
-
-              ListTile(
-                title: const Text("Vivien",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,)),
-                onTap: (){
-                  setState(() {
-                    val="Vivien";
-                  });
-                },
-              ),
-
-              ListTile(
-                title: const Text("Diane",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,)),
-                onTap: (){
-                  setState(() {
-                    val="Diane";
-                  });
-                },
-              ),
-
-              ListTile(
-                title: const Text("Caleb",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,)),
-                onTap: (){
-                  setState(() {
-                    val="Caleb";
-                  });
-                },
-              ),
-              ListTile(
-                title: const Text("Bernadin",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,)),
-                onTap: (){
-                  setState(() {
-                    val="Bernadin";
-                  });
-                },
-              ),
-          ],
-        ),
-        ),
     );
   }
 }
