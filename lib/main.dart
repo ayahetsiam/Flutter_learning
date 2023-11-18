eb0a70d63bbc0da7782b3ab0cc482261bb681353
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2023-11-18 03:34:03 +0000

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -32,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("Row & column"),
+        title: const Text("Stack"),
         leading: const IconButton(
           icon: Icon(
             Icons.alarm_rounded,
@@ -48,56 +50,22 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Column
-        (
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-          ListTile(
-            leading: Icon(Icons.person, size: 40, color: Colors.black,),
-            title: Text("Le personnage",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            subtitle:
-                Text("il a eté l'homme dont on a parlé il y a de cela 2ans",
-                style: TextStyle(fontSize: 12, color: Colors.black),),
-          ),
-          Divider(color: Colors.black, height: 4,),
+      body: Center(child: Stack(alignment: Alignment.centerLeft,
+      children: [
+        Container(color: Colors.red,
+        height: 300,
+        width: 300,
+      
+        ),
 
-          ListTile(
-            leading: Icon(Icons.person, size: 40, color: Colors.black,),
-            title: Text("Quelqu'un",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            subtitle:
-                Text("il a eté l'homme dont on a parlé il y a de celà 2ans",
-                style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 145, 107, 50)),),
-          ),
-          Divider(color: Colors.black, height: 4,),
+        Container(color: Colors.green,
+        height: 30,
+        width: 200,),
 
-          ListTile(
-            leading: Icon(Icons.person, size: 40, color: Colors.black,),
-            title: Text("l'individu",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            subtitle:
-                Text("il a eté l'homme dont on a parlé il y a de celà 2ans",
-                style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 145, 107, 50)),),
-          ),
-          Divider(color: Colors.black, height: 4,),
-
-          ListTile(
-            leading: Icon(Icons.person, size: 40, color: Colors.black,),
-            title: Text("Le personnage",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-            subtitle:
-                Text("Un projet en général est une entreprise temporaire planifiée et organisée qui vise à atteindre un objectif spécifique dans des délais et des ressources définis.",
-                style: TextStyle(fontSize: 12, color: Color.fromARGB(255, 145, 107, 50)),),
-          ),
-          Divider(color: Colors.black, height: 4,),
-          
-        ]),
+        Container(color: Colors.black,
+        height: 200,
+        width: 100,),
+      ],),
       ),
     );
   }
