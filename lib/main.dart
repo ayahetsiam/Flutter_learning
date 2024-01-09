773cfe72019d46041bb0aa81d6c284bf3a726f4f
Yawavi Etsiam AYAH <ayawaetsiam2004@gmail.com>
2024-01-09 06:48:06 +0000

    BlocConsumer

--- lib/main.dart
+++ lib/main.dart
@@ -45,32 +45,31 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if(state.wasIncremented == true){
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text("Incremented"),
-                duration: Duration(seconds: 2),
-                )
-            );
-          }else{
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text("Decremented"),
-              duration: Duration(seconds: 2),)
-            );
-          }
-        },
-        child: Center(
+      body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Text(
                 'You have pushed the button this many times:',
               ),
-              BlocBuilder<CounterCubit, CounterState>(
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if(state.wasIncremented == true){
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Incremented"),
+                          duration: Duration(seconds: 2),
+                          )
+                      );
+                    }else{
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text("Decremented"),
+                        duration: Duration(seconds: 2),)
+                      );
+                    }
+                },
                 builder: (context, state) {
-                  if (state.counterValue < 0) {
+                   if (state.counterValue < 0) {
                     return Text(
                       "hummm, negative " + state.counterValue.toString(),
                       style: Theme.of(context).textTheme.headlineMedium,
@@ -104,7 +103,6 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           ),
-        ),
       ),
     );
   }
